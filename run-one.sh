5a6631df734957db541de4305e70d382cf498d28
Hanna Zdulska <zdula616@gmail.com>
2022-05-12 14:44:51 +0200

    first run all task, then repeat

--- run-one.sh
+++ run-one.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 trap "exit" INT
 
-for t in {1..20}
+for r in {1..10}
 do
-    echo "### Experiment ${1}, task ${t} ###"
-    ./run-experiment.sh -t=${t} -e=${1} -r=10
+    for t in {1..20}
+    do
+        echo "### Experiment ${1}, task ${t} ###"
+        ./run-experiment.sh -t=${t} -e=${1}
+    done
 done
-
-  
+    
